bdc9ae7c53f78d0f1abd8cc535b3e678eea421c1
kforest <forest.kevin@gmail.com>
2019-02-06 20:44:48 -0500

    Initial commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='petfinder',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Petfinder Kaggle competition',
+    author='kforest',
+    license='MIT',
+)
